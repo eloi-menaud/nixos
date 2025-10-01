5c50f9a74e8e5b2011061312977e6427b1df7a71
elwaaaah <127682820+eloi-menaud@users.noreply.github.com>
2025-10-01 14:09:21 +0200

    Update build-home.sh

--- build-home.sh
+++ build-home.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
 
-mkdir -p "/home/elwaaaah/.config/alacritty"
-cp "/etc/nixos/alacritty/alacritty.toml" "/home/elwaaaah/.config/alacritty/"
+mkdir -p "/home/eloi/.config/alacritty"
+cp "/etc/nixos/alacritty/alacritty.toml" "/home/eloi/.config/alacritty/"
 
-mkdir -p "/home/elwaaaah/.config/niri/"
-cp "/etc/nixos/niri/config.kdl" "/home/elwaaaah/.config/niri/"
+mkdir -p "/home/eloi/.config/niri/"
+cp "/etc/nixos/niri/config.kdl" "/home/eloi/.config/niri/"
 
-cp "/etc/nixos/zsh/zshrc.bash" "/home/elwaaaah/.zshrc"
+cp "/etc/nixos/zsh/zshrc.bash" "/home/eloi/.zshrc"
 
-mkdir -p "/home/elwaaaah/.config/micro/colorschemes"
-cp "/etc/nixos/micro/bindings.json" "/home/elwaaaah/.config/micro/"
-cp "/etc/nixos/micro/settings.json" "/home/elwaaaah/.config/micro/"
-cp "/etc/nixos/micro/my.micro" "/home/elwaaaah/.config/micro/colorschemes"
+mkdir -p "/home/eloi/.config/micro/colorschemes"
+cp "/etc/nixos/micro/bindings.json" "/home/eloi/.config/micro/"
+cp "/etc/nixos/micro/settings.json" "/home/eloi/.config/micro/"
+cp "/etc/nixos/micro/my.micro" "/home/eloi/.config/micro/colorschemes"
 
-mkdir -p "/home/elwaaaah/.local/bin/"
-cp "/etc/nixos/custom-bin/edit-nixos.bash" "/home/elwaaaah/.local/bin/edit-nixos"
-chmod +x "/home/elwaaaah/.local/bin/edit-nixos"
+mkdir -p "/home/eloi/.local/bin/"
+cp "/etc/nixos/custom-bin/edit-nixos.bash" "/home/eloi/.local/bin/edit-nixos"
+chmod +x "/home/eloi/.local/bin/edit-nixos"
